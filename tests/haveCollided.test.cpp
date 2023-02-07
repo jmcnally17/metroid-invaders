e866d1ffa4f91314af9fbce89bbcd6e99144abc1
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-02-07 00:24:45 +0000

    Finish x-axis conditionals
    Add the final conditional that makes the function return false when object1
    is to the right of object2.
    
    All four of these conditionals combined means that object1 and object2 must
    have some overlap in their shapes to make the function return true.

--- tests/haveCollided.test.cpp
+++ tests/haveCollided.test.cpp
@@ -66,3 +66,24 @@ TEST(haveCollided, returnsFalseForObject1ToTheLeftOfObject2)
 
   EXPECT_FALSE(haveCollided(object1, object2));
 }
+
+TEST(haveCollided, returnsFalseForObject1ToTheRightOfObject2)
+{
+  NiceMock<MockObjectInterface> object1;
+  ON_CALL(object1, getWidth())
+      .WillByDefault(Return(24));
+  ON_CALL(object1, getHeight())
+      .WillByDefault(Return(24));
+  ON_CALL(object1, getPosition())
+      .WillByDefault(Return(sf::Vector2f(450, 730)));
+
+  NiceMock<MockObjectInterface> object2;
+  ON_CALL(object2, getWidth())
+      .WillByDefault(Return(48));
+  ON_CALL(object2, getHeight())
+      .WillByDefault(Return(48));
+  ON_CALL(object2, getPosition())
+      .WillByDefault(Return(sf::Vector2f(400, 720)));
+
+  EXPECT_FALSE(haveCollided(object1, object2));
+}
