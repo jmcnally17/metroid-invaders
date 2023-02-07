39f4e4f0c54343c324439c970a2a2166493e6f53
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-02-07 00:12:50 +0000

    Finish collision detection for y-axis
    Add a second conditon stating the top of object1 must be above the bottom
    of object2 for there to be a collision.

--- src/haveCollided.cpp
+++ src/haveCollided.cpp
@@ -3,5 +3,6 @@
 bool haveCollided(ObjectInterface &object1, ObjectInterface &object2)
 {
   bool condition1 = object1.getPosition().y + object1.getHeight() > object2.getPosition().y;
-  return condition1;
+  bool condition2 = object1.getPosition().y < object2.getPosition().y + object2.getHeight();
+  return condition1 && condition2;
 }
--- tests/haveCollided.test.cpp
+++ tests/haveCollided.test.cpp
@@ -24,3 +24,24 @@ TEST(haveCollided, returnsTrueForObject1CollidingWithTopOfObject2)
 
   EXPECT_TRUE(haveCollided(object1, object2));
 }
+
+TEST(haveCollided, returnsFalseForObject1belowObject2)
+{
+  NiceMock<MockObjectInterface> object1;
+  ON_CALL(object1, getWidth())
+      .WillByDefault(Return(24));
+  ON_CALL(object1, getHeight())
+      .WillByDefault(Return(24));
+  ON_CALL(object1, getPosition())
+      .WillByDefault(Return(sf::Vector2f(400, 800)));
+
+  NiceMock<MockObjectInterface> object2;
+  ON_CALL(object2, getWidth())
+      .WillByDefault(Return(48));
+  ON_CALL(object2, getHeight())
+      .WillByDefault(Return(48));
+  ON_CALL(object2, getPosition())
+      .WillByDefault(Return(sf::Vector2f(390, 720)));
+
+  EXPECT_FALSE(haveCollided(object1, object2));
+}
