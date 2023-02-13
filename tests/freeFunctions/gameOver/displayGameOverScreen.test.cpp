41b62c0b39d2e714af581c89632ba5662662fb30
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-02-13 22:03:00 +0000

    Draw score text on game over screen

--- tests/freeFunctions/gameOver/displayGameOverScreen.test.cpp
+++ tests/freeFunctions/gameOver/displayGameOverScreen.test.cpp
@@ -8,32 +8,35 @@ TEST(displayGameOverScreen, callsClearOnTheWindow)
 {
   NiceMock<MockRenderWindow> window;
   MockText gameOverText;
+  MockText scoreText;
   MockText playAgainText;
 
   EXPECT_CALL(window, clear())
       .Times(1);
-  displayGameOverScreen(window, gameOverText, playAgainText);
+  displayGameOverScreen(window, gameOverText, scoreText, playAgainText);
 }
 
-TEST(displayGameOverScreen, drawsTheGameOverTextAndPlayAgainText)
+TEST(displayGameOverScreen, drawsTheGameOverTextAndScoreTextAndPlayAgainText)
 {
   NiceMock<MockRenderWindow> window;
   MockText gameOverText;
+  MockText scoreText;
   MockText playAgainText;
 
   EXPECT_CALL(window, draw(testing::Truly([](const sf::Drawable &drawable)
                                           { return true; })))
-      .Times(2);
-  displayGameOverScreen(window, gameOverText, playAgainText);
+      .Times(3);
+  displayGameOverScreen(window, gameOverText, scoreText, playAgainText);
 }
 
 TEST(displayGameOverScreen, callsDisplayOnTheWindow)
 {
   NiceMock<MockRenderWindow> window;
   MockText gameOverText;
+  MockText scoreText;
   MockText playAgainText;
 
   EXPECT_CALL(window, display())
       .Times(1);
-  displayGameOverScreen(window, gameOverText, playAgainText);
+  displayGameOverScreen(window, gameOverText, scoreText, playAgainText);
 }
