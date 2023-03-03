c92c5e86175713634368fab9a8390106fd3ece52
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-03 16:06:33 +0000

    Only kill Ridley if collision is true
    Add a conditional that checks if the cannon laser and ridley are colliding.

--- src/freeFunctions/game/evaluateLaserRidleyCollision.cpp
+++ src/freeFunctions/game/evaluateLaserRidleyCollision.cpp
@@ -1,11 +1,15 @@
+#include "../../../include/interfaces/collisionInterface.hpp"
 #include "../../../include/models/iLaser.hpp"
 #include "../../../include/models/iRidley.hpp"
 #include "../../../include/wrappers/iText.hpp"
 
-void evaluateLaserRidleyCollision(ILaser &cannonLaser, IRidley &ridley, int &score, IText &scoreText)
+void evaluateLaserRidleyCollision(const CollisionInterface &collision, ILaser &cannonLaser, IRidley &ridley, int &score, IText &scoreText)
 {
-  ridley.die();
-  score += ridley.getPoints();
-  scoreText.setString("Score: " + std::to_string(score));
-  cannonLaser.reset();
+  if (collision.haveCollided(cannonLaser, ridley))
+  {
+    ridley.die();
+    score += ridley.getPoints();
+    scoreText.setString("Score: " + std::to_string(score));
+    cannonLaser.reset();
+  }
 }
--- tests/freeFunctions/game/evaluateLaserRidleyCollision.test.cpp
+++ tests/freeFunctions/game/evaluateLaserRidleyCollision.test.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/game.hpp"
+#include "../../mockInterfaces/mockCollision.hpp"
 #include "../../mockModels/mockLaser.hpp"
 #include "../../mockModels/mockRidley.hpp"
 #include "../../mockModels/mockText.hpp"
@@ -8,53 +9,90 @@ using ::testing::Return;
 
 TEST(evaluateLaserRidleyCollision, killsRidleyIfCollidingWithLaser)
 {
+  NiceMock<MockCollision> collision;
   NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   NiceMock<MockText> scoreText;
 
+  ON_CALL(collision, haveCollided)
+      .WillByDefault(Return(true));
+
   EXPECT_CALL(ridley, die())
       .Times(1);
-  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
+  evaluateLaserRidleyCollision(collision, laser, ridley, score, scoreText);
 }
 
 TEST(evaluateLaserRidleyCollision, addsTheRidleyPointsToTheScore)
 {
+  NiceMock<MockCollision> collision;
   NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   NiceMock<MockText> scoreText;
 
+  ON_CALL(collision, haveCollided)
+      .WillByDefault(Return(true));
   ON_CALL(ridley, getPoints())
       .WillByDefault(Return(150));
 
-  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
+  evaluateLaserRidleyCollision(collision, laser, ridley, score, scoreText);
   EXPECT_EQ(score, 580);
 }
 
 TEST(evaluateLaserRidleyCollision, updatesTheScoreText)
 {
+  NiceMock<MockCollision> collision;
   NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   MockText scoreText;
 
+  ON_CALL(collision, haveCollided)
+      .WillByDefault(Return(true));
   ON_CALL(ridley, getPoints())
       .WillByDefault(Return(150));
 
   EXPECT_CALL(scoreText, setString("Score: 580"))
       .Times(1);
-  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
+  evaluateLaserRidleyCollision(collision, laser, ridley, score, scoreText);
 }
 
 TEST(evaluateLaserRidleyCollision, resetsTheLaser)
 {
+  NiceMock<MockCollision> collision;
   MockLaser laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   NiceMock<MockText> scoreText;
 
+  ON_CALL(collision, haveCollided)
+      .WillByDefault(Return(true));
+
   EXPECT_CALL(laser, reset())
       .Times(1);
-  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
+  evaluateLaserRidleyCollision(collision, laser, ridley, score, scoreText);
+}
+
+TEST(evaluateLaserRidleyCollision, doesNotModifyAnythingIfRidleyAndLaserAreNotColliding)
+{
+  NiceMock<MockCollision> collision;
+  MockLaser laser;
+  MockRidley ridley;
+  int score = 430;
+  MockText scoreText;
+
+  ON_CALL(collision, haveCollided)
+      .WillByDefault(Return(false));
+  ON_CALL(ridley, getPoints())
+      .WillByDefault(Return(150));
+
+  EXPECT_CALL(ridley, die())
+      .Times(0);
+  EXPECT_CALL(scoreText, setString)
+      .Times(0);
+  EXPECT_CALL(laser, reset())
+      .Times(0);
+  evaluateLaserRidleyCollision(collision, laser, ridley, score, scoreText);
+  EXPECT_EQ(score, 430);
 }
