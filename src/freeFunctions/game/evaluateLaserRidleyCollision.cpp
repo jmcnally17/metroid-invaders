6a860f2e5a087c6f1636507e34e26286de2ae0e9
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-03 15:52:02 +0000

    Reset the laser

--- src/freeFunctions/game/evaluateLaserRidleyCollision.cpp
+++ src/freeFunctions/game/evaluateLaserRidleyCollision.cpp
@@ -1,9 +1,11 @@
+#include "../../../include/models/iLaser.hpp"
 #include "../../../include/models/iRidley.hpp"
 #include "../../../include/wrappers/iText.hpp"
 
-void evaluateLaserRidleyCollision(IRidley &ridley, int &score, IText &scoreText)
+void evaluateLaserRidleyCollision(ILaser &cannonLaser, IRidley &ridley, int &score, IText &scoreText)
 {
   ridley.die();
   score += ridley.getPoints();
   scoreText.setString("Score: " + std::to_string(score));
+  cannonLaser.reset();
 }
--- tests/freeFunctions/game/evaluateLaserRidleyCollision.test.cpp
+++ tests/freeFunctions/game/evaluateLaserRidleyCollision.test.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/game.hpp"
+#include "../../mockModels/mockLaser.hpp"
 #include "../../mockModels/mockRidley.hpp"
 #include "../../mockModels/mockText.hpp"
 
@@ -7,17 +8,19 @@ using ::testing::Return;
 
 TEST(evaluateLaserRidleyCollision, killsRidleyIfCollidingWithLaser)
 {
+  NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   NiceMock<MockText> scoreText;
 
   EXPECT_CALL(ridley, die())
       .Times(1);
-  evaluateLaserRidleyCollision(ridley, score, scoreText);
+  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
 }
 
 TEST(evaluateLaserRidleyCollision, addsTheRidleyPointsToTheScore)
 {
+  NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   NiceMock<MockText> scoreText;
@@ -25,12 +28,13 @@ TEST(evaluateLaserRidleyCollision, addsTheRidleyPointsToTheScore)
   ON_CALL(ridley, getPoints())
       .WillByDefault(Return(150));
 
-  evaluateLaserRidleyCollision(ridley, score, scoreText);
+  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
   EXPECT_EQ(score, 580);
 }
 
 TEST(evaluateLaserRidleyCollision, updatesTheScoreText)
 {
+  NiceMock<MockLaser> laser;
   NiceMock<MockRidley> ridley;
   int score = 430;
   MockText scoreText;
@@ -38,6 +42,19 @@ TEST(evaluateLaserRidleyCollision, updatesTheScoreText)
   ON_CALL(ridley, getPoints())
       .WillByDefault(Return(150));
 
-  EXPECT_CALL(scoreText, setString("Score: 580"));
-  evaluateLaserRidleyCollision(ridley, score, scoreText);
+  EXPECT_CALL(scoreText, setString("Score: 580"))
+      .Times(1);
+  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
+}
+
+TEST(evaluateLaserRidleyCollision, resetsTheLaser)
+{
+  MockLaser laser;
+  NiceMock<MockRidley> ridley;
+  int score = 430;
+  NiceMock<MockText> scoreText;
+
+  EXPECT_CALL(laser, reset())
+      .Times(1);
+  evaluateLaserRidleyCollision(laser, ridley, score, scoreText);
 }
