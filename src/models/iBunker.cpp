8ad3403eeeee4980e62105bfc42f702da979fdc3
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-03-09 00:18:22 +0000

    Add empty constructor

--- src/models/iBunker.cpp
+++ src/models/iBunker.cpp
@@ -1,3 +1,5 @@
 #include "../../include/models/iBunker.hpp"
 
+IBunker::IBunker() {}
+
 IBunker::IBunker(float width, float height, float x, float y, ISprite *sprite) : ObjectInterface(width, height, x, y, sprite) {}
